4aa3e669c8ca31387dec32f40489a3fbfe639028
Sheena Bandy <sheenabandy@gmail.com>
2013-03-27 21:12:08 -0500

    fix my connection to select from the table name instead of the database name

--- processor.php
+++ processor.php
@@ -7,9 +7,10 @@ if(isset(
 		$username = str_replace(array('\'', '"'),'',$username);
 		$password = htmlentities($_POST['password']);
 		$password = str_replace(array('\'', '"'),'',$password);
-		$connect = mysql_connect("localhost","sheenaba","v120b0PtBw","sheenaba_frozen");
-		$userinfo = mysql_query("select * from sheenaba_frozen where username='$username'")
-		or die();
+		$connect = mysql_connect("localhost","sheenaba","v120b0PtBw");
+		mysql_select_db("sheenaba_frozen") or die(mysql_error());
+		$userinfo = mysql_query("select * from frozen where username='$username'")
+		or die(mysql_error());
 		while($user = mysql_fetch_array($userinfo)){
 			if($password == $user['password']){echo "success!";}
 			else {
