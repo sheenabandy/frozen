542ea1c4d61c825a9bee41cb49a01c0f9e75a251
Sheena Bandy <sheenabandy@gmail.com>
2013-03-27 20:48:34 -0500

    search the database for the username

--- processor.php
+++ processor.php
@@ -7,14 +7,23 @@ if(isset(
 		$username = str_replace(array('\'', '"'),'',$username);
 		$password = htmlentities($_POST['password']);
 		$password = str_replace(array('\'', '"'),'',$password);
+
+		$userinfo = mysql_query("select * from sheenaba_frozen where username='$username'")
+		or die(echo "user does not exist";);
+
 	}
 else if(isset(
 	$_POST['register']
 	))
 	{
 		$username = htmlentities($_POST['user']);
+		$username = str_replace(array('\'', '"'),'',$username);
 		$email = htmlentities($_POST['email']);
+		$email = str_replace(array('\'', '"'),'',$email);
 		$password = htmlentities($_POST['password']);
+		$password = str_replace(array('\'', '"'),'',$password);
+
+
 	}
 else {
 	header('location: /frozen/login.php');
