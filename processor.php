52826e9b928398660e116bc9075f3d270c902429
Sheena Bandy <sheenabandy@gmail.com>
2013-03-27 20:24:44 -0500

    added the variables for the form data

--- processor.php
+++ processor.php
@@ -1,10 +1,20 @@
 <?php
 
-if(isset($_POST['submit'])){}
-
-else if(isset($_POST['register'])){}
-
-else {header('location: /frozen/login.php');}
-
-
+if(isset(
+	$_POST['submit']
+	)){
+		$username = $_POST['user'];
+		$password = $_POST['password'];
+	}
+else if(isset(
+	$_POST['register']
+	))
+	{
+		$username = $_POST['user'];
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+	}
+else {
+	header('location: /frozen/login.php');
+}
 ?>
