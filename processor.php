7426276501947921470a31aa8b961bd8b8c86fd2
Sheena Bandy <sheenabandy@gmail.com>
2013-03-27 20:32:18 -0500

    html entities on the form data

--- processor.php
+++ processor.php
@@ -3,16 +3,16 @@
 if(isset(
 	$_POST['submit']
 	)){
-		$username = $_POST['user'];
-		$password = $_POST['password'];
+		$username = htmlentities($_POST['user']);
+		$password = htmlentities($_POST['password']);
 	}
 else if(isset(
 	$_POST['register']
 	))
 	{
-		$username = $_POST['user'];
-		$email = $_POST['email'];
-		$password = $_POST['password'];
+		$username = htmlentities($_POST['user']);
+		$email = htmlentities($_POST['email']);
+		$password = htmlentities($_POST['password']);
 	}
 else {
 	header('location: /frozen/login.php');
