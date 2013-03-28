9aea9ffcd2850884ff52d81b3d5e567bd2b1bb38
Sheena Bandy <sheenabandy@gmail.com>
2013-03-27 23:39:16 -0500

    set up links to verify and unverify the users

--- admin.php
+++ admin.php
@@ -1,19 +1,28 @@
 <?php include('include.php');
 
+
+
 if(isset($_SESSION['admin'])){
 
+if(isset(
+	$_GET['user']
+	))
+	{
+		$id=$_GET['user'];
+		$member=$_get['member'];
+			mysql_query("update frozen set verified='1' where id='$id'");
+}
 $users = mysql_query("select * from frozen");
 
-		echo "<form name='adminform' action='' method='post'><table><tr><td>Username</td><td>Email</td><td>Member</td><td>Admin</td></tr>";
+
+		echo "<table><tr><td>Username</td><td>Email</td><td>Member</td></tr>";
 			while($user = mysql_fetch_array($users)){
 			$member = "";
-			$admin = "";
-			if($user['verified'] == "1"){$member = "checked";}
-			if($user['admin'] == "1"){$admin = "checked";}
-			echo "<tr><td>".$user['username']."</td><td>".$user['email']."</td><td><input type='checkbox' name='verified".$user['id']."' value='1' $member ></td><td><input type='checkbox' name='admin".$user['id']."' value='1' $admin ></td></tr>";
+			if($user['verified'] == "1"){$member = "0"; $linktext="unverify";} else {$member = '1'; $linktext="verify";}
+			echo "<tr><td>".$user['username']."</td><td>".$user['email']."</td><td><a href='?user=".$user['id']."&member=$member'>$linktext</a></td></tr>";
 
 			}
-		echo "<tr><td colspan='4' align='right'><input type='submit' name='submit' value='Change'></td></tr></table></form>";
+		echo "</table>";
 
 
 } else {echo "access denied";}
