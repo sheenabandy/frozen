4ecad5f64512080c655872a9dafe90a912e7c9cd
Sheena Bandy <sheenabandy@gmail.com>
2013-03-27 22:28:44 -0500

    added a link to the members page and added a table to display members

--- admin.php
+++ admin.php
@@ -1,1 +1,19 @@
-<?php include('include.php');?>
+<?php include('include.php');
+
+if(isset($_SESSION['admin'])){
+
+$users = mysql_query("select * from frozen");
+		if (mysql_num_rows($userinfo)!=0) {
+		echo "<table><tr><td>Username</td><td>Email</td><td>Member</td><td>Admin</td></tr>";
+			while($user = mysql_fetch_array($users)){
+			if($user['verified']){$member = "checked";}
+			if($user['admin']){$admin = "checked";}
+			echo "<tr><td>".$user['username']."</td><td>".$user['email']."</td><td><input type='checkbox' name='verified".$user['id']."' value='1' $member ></td><td><input type='checkbox' name='admin".$user['id']."' value='1' $admin ></td></tr>";
+
+			}
+		echo "</table>";
+		}
+
+} else {echo "access denied";}
+
+?>
--- members.php
+++ members.php
@@ -2,7 +2,13 @@
 if(isset($_SESSION['loggedin'])){
 ?>
 <h2>Coming Soon</h2>
-<?php } else {
+<?php
+if(isset($_SESSION['admin'])){
+?>
+<a href="/frozen/admin.php">Edit Members</a>
+<?php
+}
+} else {
 header("location:/frozen/login.php");
 }
 ?>
