a9e04df34e640616cbc9dec643532238d21bd664
Sheena Bandy <sheenabandy@gmail.com>
2013-03-27 22:38:17 -0500

    the admin member form is ready to be processed

--- admin.php
+++ admin.php
@@ -4,14 +4,16 @@ if(isset($_SESSION['admin'])){
 
 $users = mysql_query("select * from frozen");
 
-		echo "<table><tr><td>Username</td><td>Email</td><td>Member</td><td>Admin</td></tr>";
+		echo "<form name='adminform' action='' method='post'><table><tr><td>Username</td><td>Email</td><td>Member</td><td>Admin</td></tr>";
 			while($user = mysql_fetch_array($users)){
-			if($user['verified']){$member = "checked";}
-			if($user['admin']){$admin = "checked";}
+			$member = "";
+			$admin = "";
+			if($user['verified'] == "1"){$member = "checked";}
+			if($user['admin'] == "1"){$admin = "checked";}
 			echo "<tr><td>".$user['username']."</td><td>".$user['email']."</td><td><input type='checkbox' name='verified".$user['id']."' value='1' $member ></td><td><input type='checkbox' name='admin".$user['id']."' value='1' $admin ></td></tr>";
 
 			}
-		echo "</table>";
+		echo "<tr><td colspan='4' align='right'><input type='submit' name='submit' value='Change'></td></tr></table></form>";
 
 
 } else {echo "access denied";}
